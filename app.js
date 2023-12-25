be5d25f8480b219d8997087dd1c4250ab137ea0d
shuke8 <stemirov777@gmail.com>
2023-12-25 10:53:51 +0500

    link changed

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ signInForm.addEventListener("submit", async function (e) {
 
     if (validCredential) {
       // managers
-      window.location.href = "https://portal-atg.vercel.app/";
+      window.location.href = "https://atg-managers.vercel.app/";
     } else {
       // engeneers
-      window.location.href = "https://portal-atg.vercel.app/";
+      window.location.href = "https://atg-managers.vercel.app/";
     }
   } catch (error) {
     // Handle Firebase authentication errors
